163fb1fe90e593974158ace47c2a57538901a276
Marcin Walczak <marcin.walczak@gmail.com>
2020-06-24 11:18:31 +0100

    Completed export option

---
+++ lib/my_hive/time_sheet/services/duration_calculator.ex
@@ -0,0 +1,39 @@
+defmodule MyHive.TimeSheet.Services.DurationCalculator do
+  alias MyHive.TimeSheet.TimeEntry
+  alias Timex.Format.Duration
+
+  @one_minute 60
+  @one_hour 3600
+
+  def call(entries) do
+    Enum.map(entries, fn en ->
+      TimeEntry.duration_in_seconds(en) |> abs()
+    end)
+      |> sum_list()
+      |> to_hh_mm()
+  end
+
+  def sum_list(list), do: Enum.reduce(list, 0, &(&1 + &2))
+
+  def to_hh_mm(seconds) when seconds >= @one_hour do
+    h = div(seconds, @one_hour)
+
+    m =
+      seconds
+      |> rem(@one_hour)
+      |> div(@one_minute)
+      |> pad_int()
+
+    "#{h}:#{m}"
+  end
+
+  def to_hh_mm(seconds) do
+    div(seconds, @one_minute)
+  end
+
+  defp pad_int(int, padding \\ 2) do
+    int
+    |> Integer.to_string()
+    |> String.pad_leading(padding, "0")
+  end
+end
