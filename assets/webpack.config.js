370dd712110f220bfae1579099b55c075c400a79
Marcin Walczak <marcin.walczak@gmail.com>
2020-07-09 09:49:19 +0100

    Fix font attempt

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (env, options) => ({
             options: {
               esModule: false,
               name: '[name].[ext]',
-              outputPath: 'fonts/'
+              outputPath: '../fonts/'
             }
           }
       },  
