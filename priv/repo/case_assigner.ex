84e5524bb6ab2ad5047b30cdcb16e301f276bec4
Marcin Walczak <marcin.walczak@gmail.com>
2020-09-11 22:14:34 +0100

    Fix eidt

--- priv/repo/case_assigner.ex
+++ priv/repo/case_assigner.ex
@@ -1,30 +1,37 @@
 defmodule MyHive.CaseAssigner do
   alias MyHive.CaseManagement
   alias MyHive.Accounts
+  alias MyHive.Repo
+  alias MyHive.FileManager
 
   def call(prof_id) do
     profs_cases = CaseManagement.cases_with_user(prof_id)
-    sophie = Accounts.get_user!(42)
-    anthony = Accounts.get_user!(18)
-    viv = Accounts.get_user!(7)
-    mar = Accounts.get_user!(27)
+    sophie = Accounts.get_user!(42) |> Repo.preload(:shared_folders_by_others)
+    anthony = Accounts.get_user!(18) |> Repo.preload(:shared_folders_by_others)
+    viv = Accounts.get_user!(7) |> Repo.preload(:shared_folders_by_others)
     Enum.each(profs_cases, fn mlc ->
-      if !has_user?(mlc, sophie) do
-        CaseManagement.add_to_user_to_case(sophie, mlc)
+      if !has_shared_folder?(mlc, sophie) do
+        FileManager.share_folder(mlc.folder_id, mlc.user_id, sophie.id)
       end
-      if !has_user?(mlc, anthony) do
-        CaseManagement.add_to_user_to_case(anthony, mlc)
+      if !has_shared_folder?(mlc, anthony) do
+        FileManager.share_folder(mlc.folder_id, mlc.user_id, anthony.id)
       end
-      if !has_user?(mlc, viv) do
-        CaseManagement.add_to_user_to_case(viv, mlc)
+      if !has_shared_folder?(mlc, viv) do
+        FileManager.share_folder(mlc.folder_id, mlc.user_id, viv.id)
       end
-      CaseManagement.del_user_medico_legal_cases(mlc, [27])
     end)
   end
 
-  def has_user?(mlc, user) do
-    Enum.member?(mlc.users, user)
+  def has_shared_folder?(mlc, user) do
+    folder = MyHive.FileManager.get_folder!(mlc.folder_id)
+    ids = ids_from_query_collection(user.shared_folders_by_others)
+    Enum.member?(ids, folder.id)
   end
+
+  defp ids_from_query_collection(collection) do
+    Enum.map(collection, fn f -> f.id end)
+  end
+
 
 end
 MyHive.CaseAssigner.call(System.get_env("US_ID"))
