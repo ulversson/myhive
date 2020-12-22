26886fc32a0e7e618f3c9aa53b11809de09c117d
Marcin Walczak <marcinwalczak@Marcins-MacBook-Pro.local>
2020-12-22 21:26:37 +0000

    St

--- lib/my_hive/case_management/services/timeline_organizer.ex
+++ lib/my_hive/case_management/services/timeline_organizer.ex
@@ -11,6 +11,7 @@ defmodule MyHive.CaseManagement.Services.TimelineOrganizer do
       |> Enum.each(fn({stage, index}) ->
         order = index
         stage =  stage |> elem(1)
+        IO.puts("Stage: #{stage["name"]}, index: #{index}, order: #{stage["order"]}")
         stage["id"]
           |> CaseManagement.find_status_by_id()
           |> MedicoLegalCaseStatus.changeset(%{order: order})
