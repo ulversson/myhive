1bdab400a606b63119308f6cd4473c8edaa2adbe
Marcin Walczak <marcin.walczak@gmail.com>
2021-12-07 14:56:26 +0000

    Modality name

--- assets/js/client/radiology/components/ModalityColumn.vue
+++ assets/js/client/radiology/components/ModalityColumn.vue
@@ -1,8 +1,9 @@
 <template>
   <div class='radiology-modality' style='font-size: 16px'>
     <a class='badge badge-pill text-white'
-      :style="`background-color: ${modality.color} !important`">
-      {{$attrs.data['Modalities in Study']}}
+      :style="`background-color: ${modality.color} !important`" data-toggle="tooltip"
+      :data-title="modality.name" :title="modality.name">
+      {{$attrs.data['Modalities in Study']}}&nbsp;-&nbsp;{{modality.name}}
     </a>
   </div>
 </template>
@@ -16,7 +17,6 @@ export default {
     modality() {
       const modalitiesShort = Object.keys(this.modalities)
       const modalityName = modalitiesShort.filter(m => m == this.$attrs.data["Modalities in Study"].trim())
-      console.log(modalityName)
       return this.modalities[modalityName]
     }
   },
