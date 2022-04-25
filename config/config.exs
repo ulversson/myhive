cc335fa5718e85436f10b67eeeca9b3b26c829d7
Marcin Walczak <marcin.walczak@gmail.com>
2022-04-25 14:40:23 +0100

    Email config

--- config/config.exs
+++ config/config.exs
@@ -61,15 +61,15 @@ config :my_hive, MyHive.Guardian,
   secret_key: "A1eajRxbefshwZvqLM6NZB8ioCJIQ9nsmX09YBz0WWM6RJYkCPdEwuaDkHqOvbOY"
 
 config :my_hive, MyHive.Mailer,
-  adapter: Bamboo.SMTPAdapter,
-  server: "smtp.gmail.com",
-  port: 587,
-  username:  "info@my-hive.co.uk",
-  password:  {:system, "EMAIL_PASS"},
-  tls: :if_available, # can be `:always` or `:never`
-  allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"], # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
-  ssl: false, # can be `true`
-  retries: 1
+  adapter: Bamboo.SendGridAdapter,
+  api_key: System.get_env("SENDGRID_API_KEY"),
+  hackney_opts: [
+    recv_timeout: :timer.minutes(1),
+    connect_timeout: :timer.minutes(1),
+    ssl_options: [
+      versions: [:"tlsv1.2"]
+    ]
+  ]
 
 config :phoenix,
   template_engines: [leex: Phoenix.LiveView.Engine]
